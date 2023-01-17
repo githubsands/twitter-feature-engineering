79d52d1458dd2754f8157847f633fd5d83e50e58
Ryan Vacek <ryanvacek@Ryans-MacBook-Pro.local>
2023-01-17 12:48:27 -0700

    /feature adds some design changes and proxy routing

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-# https://docs.tweepy.org/en/stable/client.html#
-
+# DOCS: https://docs.tweepy.org/en/stable/client.html#
 import os
 import logging
 import colorlog
 import tweepy
 
-APIKEY=os.environ.get("APIKEY")
-APIKEY_SECRET=os.environ.get("APIKEY_SECRET")
-BEARER_TOKEN=os.environ.get("BEARER_TOKEN")
-ACCESS_TOKEN=os.environ.get("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET=os.environ.get("ACCESS_TOKEN_SECRET")
-USER=os.environ.get("USER")
+# TODO: You should add multiplie proxies and each submission through tweepy needs to alternate
+# our servers
+os.environ['HTTP_PROXY'] = ""
+os.environ['HTTPS_PROXY'] = ""
 
 class twitter_client():
-    def __init__(self):
+    def __init__(self, BEARER_TOKEN, APIKEY, APIKEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET):
         try:
             client = tweepy.Client(bearer_token=BEARER_TOKEN,consumer_key=APIKEY,consumer_secret=APIKEY_SECRET,access_token=ACCESS_TOKEN,access_token_secret=ACCESS_TOKEN_SECRET)
+            print(BEARER_TOKEN)
             auth = tweepy.OAuth2BearerHandler(BEARER_TOKEN)
+            print(auth)
             api = tweepy.API(auth,wait_on_rate_limit=True)
         except ValueError as e:
             print("received error", e)
         else:
             self.client=client
             self.api=api
+            print("authentication succeeded")
 
 class follower_manager(twitter_client):
     def __init__(self):
@@ -42,15 +42,19 @@ class follower_manager(twitter_client):
                     else:
                         print("unfollowed succeeded")
                         break
+    # def list_friends(self)
 
 class list_manager(twitter_client):
-    def __init__(self):
-        twitter_client.__init__(self)
+    def __init__(self, BEARER_TOKEN, APIKEY, APIKEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET):
+        twitter_client.__init__(self, BEARER_TOKEN, APIKEY, APIKEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         pass
     def get_tweets_in_list(self,list_id):
         tweets = self.client.get_list_tweets(list_id)
     def add_steady_feed(self):
         resp = self.client.get_list_members(id=12)
+    def get_my_lists(self):
+        lists = self.api.get_lists()
+        print(lists)
 
 class spaces_manager(twitter_client):
     def __init__(self):
@@ -58,6 +62,12 @@ class spaces_manager(twitter_client):
         pass
 
 def main():
+    APIKEY=os.environ.get("APIKEY")
+    APIKEY_SECRET=os.environ.get("APIKEY_SECRET")
+    BEARER=os.environ.get("BEARER")
+    ACCESS_TOKEN=os.environ.get("ACCESS_TOKEN")
+    ACCESS_TOKEN_SECRET=os.environ.get("ACCESS_TOKEN_SECRET")
+    USER=os.environ.get("USER")
     logger=colorlog.getLogger()
     handler = colorlog.StreamHandler()
     formatter = colorlog.ColoredFormatter(
@@ -74,23 +84,24 @@ def main():
     )
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-    if APIKEY == "":
+    if APIKEY == None:
         print("must set apikey")
         sys.exit()
-    elif APIKEY_SECRET == "":
+    elif APIKEY_SECRET == None:
         print("must set apikey_secret")
         sys.exit()
-    elif BEARER_TOKEN == "":
+    elif BEARER == None:
         print("must set bearer token")
         sys.exit()
-    elif ACCESS_TOKEN == "":
+    elif ACCESS_TOKEN == None:
         print("must set access token")
         sys.exit()
-    elif ACCESS_TOKEN_SECRET == "":
+    elif ACCESS_TOKEN_SECRET == None:
         print("must set access token secret")
         sys.exit()
     logger.info("environmental variables set")
-    lm=list_manager()
+    lm=list_manager(BEARER,APIKEY,APIKEY_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+    lm.get_my_lists()
 
 if __name__ == "__main__":
     main()
