f18315e082cbb89e3229ac4206dcf8ea19510905
Ryan Vacek <ryanvacek@Ryans-MacBook-Pro.local>
2023-03-19 11:31:42 -0600

    /feature adds get all tweets from a list through pagination

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # DOCS: https://docs.tweepy.org/en/stable/client.html#
 import os
 import logging
-import colorlog
 import tweepy
 import sys
+import json
+
+STEADY_FEED_ID=1530640892276551680
 
 # TODO: You should add multiplie proxies and each submission through tweepy needs to alternate
 # our servers
@@ -45,6 +47,7 @@ class people_manager(twitter_client):
                         print("unfollowed succeeded")
                         break
     def list_friends(self):
+        print("USER IS " + self.user)
         friends = self.api.get_follower_ids(user_id=self.user)
         for friend in friends:
             print(friends)
@@ -53,11 +56,27 @@ class list_manager(twitter_client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     def get_tweets_in_list(self,list_id):
-        tweets = self.client.get_list_tweets(list_id)
-    def add_steady_feed(self):
-        resp = self.client.get_list_members(id=12)
+        tweets = []
+        try:
+            tweet_obj_origin = self.client.get_list_tweets(id=list_id)
+        except ValeuError as e:
+            print("received error", e)
+        else:
+             tweets.extend(tweet_obj_origin.data)
+             while "next_token" in tweet_obj_origin.meta:
+                next_token = tweet_obj_origin.meta["next_token"]
+                try:
+                    tweet_obj = self.client.get_list_tweets(id=list_id, pagination_token=next_token)
+                except ValueError as e:
+                    print("Received error:", e)
+                    break
+                else:
+                    tweets.extend(tweet_obj.data)
+                    tweet_obj_origin = tweet_obj
+        print(tweets)
     def get_my_lists(self):
-        lists = self.api.get_lists()
+        self.client.get_list_tweets()
+        lists = self.api.get_lists(user=self.user)
         print(lists)
 
 class spaces_manager(twitter_client):
@@ -66,13 +85,22 @@ class spaces_manager(twitter_client):
         pass
 
 def main():
-    USER=os.environ.get("USER")
+    logging.basicConfig(level=logging.INFO)
+
+    USERNAME=os.environ.get("TWITTERHANDLER")
     APIKEY=os.environ.get("APIKEY")
     APIKEY_SECRET=os.environ.get("APIKEY_SECRET")
     BEARER_TOKEN=os.environ.get("BEARER_TOKEN")
     ACCESS_TOKEN=os.environ.get("ACCESS_TOKEN")
     ACCESS_TOKEN_SECRET=os.environ.get("ACCESS_TOKEN_SECRET")
-    USER=os.environ.get("USER")
+
+    logging.info(f"APIKEY: {APIKEY}")
+    logging.info(f"APIKEY_SECRET: {APIKEY_SECRET}")
+    logging.info(f"BEARER_TOKEN: {BEARER_TOKEN}")
+    logging.info(f"USERNAME: {USERNAME}")
+    logging.info(f"ACCESS_TOKEN: {ACCESS_TOKEN}")
+    logging.info(f"ACCESS_TOKEN_SECRET: {ACCESS_TOKEN_SECRET}")
+
     if APIKEY == None:
         print("must set apikey")
         sys.exit()
@@ -88,12 +116,12 @@ def main():
     elif ACCESS_TOKEN_SECRET == None:
         print("must set access token secret")
         sys.exit()
-    elif USER == None:
-        print("must set access token secret")
+    elif USERNAME == None:
+        print("must set user")
         sys.exit()
 
-    pm=people_manager(USER, BEARER_TOKEN, APIKEY, APIKEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    pm.list_friends()
+    pm=list_manager(USERNAME, BEARER_TOKEN, APIKEY, APIKEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    pm.get_tweets_in_list(1163779260865306625)
 
 if __name__ == "__main__":
     main()
